5f7dfefe202d6c0c37e6c70769d474ac3e1fbf1b
HazemAshraf14 <hazem.ashraf@thirdwayv.com>
2023-02-16 19:09:53 +0200

    initial commit#1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.get('/' , (req,res) => res.send('<h1>Hello Hazem! kkkk</h1>'))
+app.get('/' , (req,res) => res.send('<h1>Hello Hazem! kkkkkkkkk</h1>'))
 app.listen(PORT,() => console.log(`app is up and running on port: ${PORT}`));
